86220b10b3a5312cc614e66889cac5bd23da0b04
wen kai <clwk94.work@gmail.com>
2021-08-07 22:32:47 +0800

    fix: update environment.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: "https://heroku-fastevent.herokuapp.com/",
+  firebaseConfig: {
+    apiKey: "AIzaSyD2MEi7RDByvGqTuRnStCsLfKi8N_pwxLA",
+    authDomain: "fastevent-fec5c.firebaseapp.com",
+    projectId: "fastevent-fec5c",
+    storageBucket: "fastevent-fec5c.appspot.com",
+    messagingSenderId: "873060476035",
+    appId: "1:873060476035:web:dc520336e540a4d87127b6"
+  }
 };
